dd99805551f24b7efb4d9e7ab35068046fe8d0cf
Morten Enemark Lund <mel@anybodytech.com>
2025-06-10 09:28:39 +0200

    fix typing issues

--- sillywalk/mlpca.py
+++ sillywalk/mlpca.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on May 31 2025
 
@@ -7,31 +6,35 @@ This is John's implementation of the sillywalk algorithm.
 @author: jr
 """
 
-from typing import Dict, Optional, Union
+from collections.abc import Mapping, Sequence
 
 import narwhals as nw
 import numpy as np
 from narwhals.typing import IntoFrame
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
+from numpy.typing import NDArray
+from sklearn.decomposition import PCA  # type: ignore[import-untyped]
+from sklearn.preprocessing import StandardScaler  # type: ignore[import-untyped]
+
+# Type Alias Definition
+NumericSequenceOrArray = Sequence[float | int] | NDArray[np.floating | np.integer]
+StringSequenceOrArray = Sequence[str] | NDArray[np.str_]
 
 
 class PCAPredictor:
-
     def __baseinit__(
         self,
-        all_means: list[float] | np.ndarray,
-        all_stds: list[float] | np.ndarray,
-        all_columns: list[str] | np.ndarray,
-        pca_columns: list[str] | np.ndarray,
-        pca_components: list[float] | np.ndarray,
-        pca_eigenvalues: list[float] | np.ndarray,
+        all_means: NumericSequenceOrArray,
+        all_stds: NumericSequenceOrArray,
+        all_columns: StringSequenceOrArray,
+        pca_columns: StringSequenceOrArray,
+        pca_components: NumericSequenceOrArray,
+        pca_eigenvalues: NumericSequenceOrArray,
     ):
         if isinstance(all_columns, np.ndarray):
             all_columns = all_columns.tolist()
         if isinstance(pca_columns, np.ndarray):
             pca_columns = pca_columns.tolist()
-            
+
         self.all_means = dict(zip(all_columns, np.array(all_means)))
         self.all_stds = dict(zip(all_columns, np.array(all_stds)))
         self.all_columns = all_columns
@@ -42,38 +45,37 @@ class PCAPredictor:
         self._low_variance_columns = set(all_columns).difference(pca_columns)
         self._mean_vec = np.array([self.all_means[col] for col in self.pca_columns])
         self._std_vec = np.array([self.all_stds[col] for col in self.pca_columns])
-        self._eigenvalues = pca_eigenvalues
-        self._U_k = pca_components.T
-        self.y_opt = None
+        self._eigenvalues = np.array(pca_eigenvalues)
+        self._U_k = self.pca_components.T
+        self.y_opt: NDArray | None = None
 
     @classmethod
     def from_pca(
         cls,
-        all_means: np.ndarray,
-        all_stds: np.ndarray,
-        all_columns: list[str],
-        pca_columns: list[str],
-        pca_components: np.ndarray,
-        pca_eigenvalues: np.ndarray,
+        all_means: NumericSequenceOrArray,
+        all_stds: NumericSequenceOrArray,
+        all_columns: StringSequenceOrArray,
+        pca_columns: StringSequenceOrArray,
+        pca_components: NDArray,
+        pca_eigenvalues: NDArray,
     ):
-        instance = cls.__new__(cls, all_means, all_stds, all_columns, pca_columns, pca_components, pca_eigenvalues)
+        instance = cls.__new__(cls)
         instance.__baseinit__(
             all_means=all_means,
             all_stds=all_stds,
             all_columns=all_columns,
             pca_columns=pca_columns,
             pca_components=pca_components,
-            pca_eigenvalues=pca_eigenvalues
+            pca_eigenvalues=pca_eigenvalues,
         )
         return instance
-
 
     def __init__(
         self,
         df_native: IntoFrame,
-        n_components: Union[int, None] = None,
+        n_components: int | None = None,
         variance_threshold: float = 1e-8,
-        relative_variance_ratio: float = 1e-3
+        relative_variance_ratio: float = 1e-3,
     ):
         df = nw.from_native(df_native)
 
@@ -87,7 +89,12 @@ class PCAPredictor:
         stds = df.select(nw.all().std()).to_numpy().flatten()
         relative_ratios = stds / (means + 1e-12)
 
-        pca_columns = all_columns[np.logical_and(variances >= variance_threshold, relative_ratios >= relative_variance_ratio)]
+        pca_columns = all_columns[
+            np.logical_and(
+                variances >= variance_threshold,
+                relative_ratios >= relative_variance_ratio,
+            )
+        ]
         df_reduced = df.select(pca_columns)
 
         X_scaled = StandardScaler().fit_transform(df_reduced)
@@ -97,24 +104,24 @@ class PCAPredictor:
 
         pca = PCA(n_components=n_components)
         pca.fit(X_scaled)
-        
+
         self.__baseinit__(
             all_means=means,
             all_stds=stds,
             all_columns=df.columns,
             pca_columns=pca_columns,
             pca_components=pca.components_,
-            pca_eigenvalues=pca.explained_variance_
+            pca_eigenvalues=pca.explained_variance_,
         )
 
-    def _drop_parallel_constraints(self, B: np.ndarray, d: dict[str, float]) -> tuple:
+    def _drop_parallel_constraints(self, B: NDArray, d: dict[str, float]) -> tuple:
         drop: list[str] = []
         for i in range(B.shape[0]):
             for j in range(i + 1, B.shape[0]):
                 inner_product = np.inner(B[i], B[j])
                 norm_i = np.linalg.norm(B[i])
                 norm_j = np.linalg.norm(B[j])
-                if np.abs(inner_product - norm_j * norm_i) < 1E-7:
+                if np.abs(inner_product - norm_j * norm_i) < 1e-7:
                     drop.append(list(d)[j])
         drop = list(set(drop))
         drop_indices = [list(d).index(key) for key in drop]
@@ -124,23 +131,32 @@ class PCAPredictor:
 
     def predict(
         self,
-        constraints: Dict[str, float],
-        target_pcs: Optional[np.ndarray] = None
-    ) -> dict[str, float]:
-        
-        low_variance_constraints = [col for col in constraints if col in self._low_variance_columns]
+        constraints: Mapping[str, float | int],
+        target_pcs: np.ndarray | None = None,
+    ) -> dict[str, float | int]:
+        low_variance_constraints = [
+            col for col in constraints if col in self._low_variance_columns
+        ]
         if low_variance_constraints:
-            raise ValueError(f"Constraint cannot be applied to excluded low-variance columns: {low_variance_constraints}")
+            raise ValueError(
+                f"Constraint cannot be applied to excluded low-variance columns: {low_variance_constraints}"
+            )
 
         # constraint_indices = [self.pca_columns.get_loc(var) for var in constraints.keys()]
-        constraint_indices = np.array([self.pca_columns[var] for var in constraints if var in self.pca_columns])
+        constraint_indices = np.array(
+            [self.pca_columns[var] for var in constraints if var in self.pca_columns]
+        )
 
         standardized_constraints = {}
         for var in constraints:
             if var not in self.pca_columns:
-                raise ValueError(f"Constraint variable '{var}' is not part of the PCA columns.")
+                raise ValueError(
+                    f"Constraint variable '{var}' is not part of the PCA columns."
+                )
             idx = self.pca_columns[var]
-            standardized_constraints[var] = (constraints[var] - self._mean_vec[idx]) / self._std_vec[idx]
+            standardized_constraints[var] = (
+                constraints[var] - self._mean_vec[idx]
+            ) / self._std_vec[idx]
 
         # standardized_constraints = [
         #     (val - self._mean_vec[i]) / self._std_vec[i] for i, val in zip(constraint_indices, constraints.values())
@@ -173,13 +189,11 @@ class PCAPredictor:
 
         full_prediction = dict()
         for col in self.all_columns:
-            if col in self.pca_columns: 
+            if col in self.pca_columns:
                 full_prediction[col] = predicted_reduced[col]
             else:
-               full_prediction[col] = self.all_means[col]
+                full_prediction[col] = self.all_means[col]
 
         self.y_opt = y_opt
 
-        return  full_prediction
-    
-
+        return full_prediction
