26ac974e4ca8db59fe4492dc9bb7fefab8ab8313
Morten Enemark Lund <mel@anybodytech.com>
2025-06-10 09:49:46 +0200

    format testing file

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,20 +1,22 @@
 import polars as pl
 
+
 def test_hard():
     import sillywalk  # noqa
 
     version = sillywalk.__version__
-    assert version == "unknown" or isinstance(version, str), f"Expected version to be 'unknown' or a string, got {version!r}"
+    assert version == "unknown" or isinstance(version, str), (
+        f"Expected version to be 'unknown' or a string, got {version!r}"
+    )
 
     df = pl.read_csv("tests/students.csv")
 
     model = sillywalk.PCAPredictor(df)
 
     f = {
-        'Sex': 2,
-        'Stature': 180,
-        'Bodyweight': 65,
+        "Sex": 2,
+        "Stature": 180,
+        "Bodyweight": 65,
     }
     row = model.predict(f)
     print(row)
-
